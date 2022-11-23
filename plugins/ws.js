82271de3e34542332f9c3af8c1ace60455d7b82b
yusefrich <richard.alves.dev@gmail.com>
2022-11-23 15:06:42 -0300

    fix: fixed all variables dynamic

--- plugins/ws.js
+++ plugins/ws.js
@@ -5,16 +5,16 @@ window.Pusher = require('pusher-js')
 export default (_, inject) => {
   if (process.env.WS_HOST && process.env.WS_KEY) {
     const echo = new Echo({
-      broadcaster: 'pusher',
+      broadcaster: process.env.WS_BROADCASTER,
       key: process.env.WS_KEY,
       secret: process.env.WS_SECRET,
       wsHost: process.env.WS_HOST,
       wsPort: process.env.WS_PORT,
       wssPort: process.env.WSS_PORT,
-      encrypted: false,
-      disableStats: true,
+      encrypted: process.env.WS_ENCRYPTED && process.env.WS_ENCRYPTED === 'true' ? true : false,
+      disableStats: process.env.WS_DISABLE_STATS && process.env.WS_DISABLE_STATS === 'true' ? true : false,
       cluster: 'mt1',
-      forceTLS: false,
+      forceTLS: process.env.WS_FORCE_TLS && process.env.WS_FORCE_TLS === 'true' ? true : false,
       enabledTransports: ['ws', 'wss'],
       disabledTransports: ['sockjs', 'xhr_polling', 'xhr_streaming']
     })
